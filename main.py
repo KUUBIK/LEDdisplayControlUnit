859b56534edea5f96fa29ab4ce53cbde28bac6e0
biggybaron <dr.cleverest@gmail.com>
2018-05-13 16:22:08 +0600

    First working update

--- main.py
+++ main.py
@@ -6,15 +6,19 @@ app = Flask(__name__)  # Creating new flask app
 
 @app.route("/1/client/", methods=['GET', 'POST'])  # Root for login page is index "/"
 def client1():
-    image = "static/clev.png"
+    with open("./picture1.txt", 'r') as file:
+        image = file.readline()
+    image = "/static/" + image
+    image = Markup(image)
     return render_template(
         "show.html", **locals())
 
 
 @app.route("/1/control/<image_path>", methods=['GET', 'POST'])  # Root for login page is index "/"
 @app.route("/1/control/", methods=['GET', 'POST'])  # Root for login page is index "/"
-def control1():
-    image = "static/clev.png"
+def control1(image_path="None"):
+    with open("./picture1.txt", "w") as file:
+        file.write(image_path + ".png")
     return render_template(
         "home.html", **locals())
 
@@ -22,4 +26,4 @@ def control1():
 # Main flask app
 if __name__ == "__main__":
     # It creates https access by last argument. It is need to be give to web-page permission to microphone
-    app.run(host='0.0.0.0', port=8090) #, ssl_context='adhoc')
+    app.run(host='192.168.8.104', port=8090, debug=True) #, ssl_context='adhoc')
