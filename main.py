967969e0f40c69781dcc34fa1e5b0931f92be5d6
CEO <bospan@cleverest.tech>
2018-05-17 23:02:17 +0600

    Finished, waiting pngs from Ali

--- main.py
+++ main.py
@@ -4,26 +4,54 @@ from flask import Flask, render_template, request, Markup
 app = Flask(__name__)  # Creating new flask app
 
 
-@app.route("/1/client/", methods=['GET', 'POST'])  # Root for login page is index "/"
-def client1():
-    with open("./picture1.txt", 'r') as file:
+@app.route("/client/<LCD>/", methods=['GET', 'POST'])  # Root for login page is index "/"
+def client(LCD):
+    with open(str("./" + LCD + ".txt"), 'r') as file:
         image = file.readline()
     image = "/static/" + image
     image = Markup(image)
     return render_template(
-        "show.html", **locals())
+        "display.html", **locals())
 
 
-@app.route("/1/control/<image_path>", methods=['GET', 'POST'])  # Root for login page is index "/"
-@app.route("/1/control/", methods=['GET', 'POST'])  # Root for login page is index "/"
-def control1(image_path="None"):
-    with open("./picture1.txt", "w") as file:
+@app.route("/control/<LCD>/<image_path>", methods=['GET', 'POST'])  # Root for login page is index "/"
+@app.route("/control/<LCD>/", methods=['GET', 'POST'])  # Root for login page is index "/"
+def control(LCD, image_path="Frame"):
+    with open(str("./" + LCD + ".txt"), "w") as file:
         file.write(image_path + ".png")
     return render_template(
-        "home.html", **locals())
+        "index.html", **locals())
+
+
+@app.route("/control/<LCD>/<image_path>/<DIR>/", methods=['GET', 'POST'])  # Root for login page is index "/"
+@app.route("/control/<LCD>/<image_path>/", methods=['GET', 'POST'])  # Root for login page is index "/"
+def controlInner(LCD, image_path="Frame", DIR = "0"):
+
+    with open(str("./" + image_path + "files.txt"), "r") as red:
+        print("Opened files")
+        files = red.readlines()
+        with open(str("./" + LCD + "now.txt"), "r") as red2:
+            print("Opened now")
+            now = int(red2.readline().replace('\n', ' ').replace('\r', ''))
+        d = int(DIR) + now
+        if d >= len(files) or d <= 0:
+            d = 0
+        with open(str("./" + LCD + "now.txt"), "w") as red3:
+            red3.write(str(d))
+        print(d)
+        image_path2 = files[d].replace('\n', '').replace('\r', '')
+        print(image_path2)
+        with open(str("./" + LCD + ".txt"), "w") as file:
+            file.write(image_path2 + ".png")
+
+
+    return render_template(
+        "inner.html", **locals())
 
 
 # Main flask app
 if __name__ == "__main__":
     # It creates https access by last argument. It is need to be give to web-page permission to microphone
-    app.run(host='192.168.8.104', port=8090, debug=True) #, ssl_context='adhoc')
+    app.run(host='192.168.2.2', port=8090, debug=True) #, ssl_context='adhoc')
+
+
